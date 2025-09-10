7bcdb3c2bc98136c38a886b1878e9c00e5dab38b
Rocky McGoey <mcgoeyrocky@gmail.com>
2025-09-10 09:10:31 -0600

    Added a print out of characters with age <40 and rendered

--- index.js
+++ index.js
@@ -31,6 +31,18 @@ for (let i = 0; i < users.length; i++) {
 
 // 2. Print out the names of characters whose age is less than 40 in the console, then render them in the HTML list with id "young-characters-list"
 
+const youngCharactersList = document.getElementById("young-characters-list");
+
+users.forEach((user) => {
+  if (user.age < 40) {
+    console.log(user.name);
+
+    const listItem = document.createElement("li");
+    listItem.textContent = user.name;
+    youngCharactersList.appendChild(listItem);
+  } 
+});
+
 // 3. Create a reusable function that takes any array and uses logic to render a list of character names in the HTML. Use this function to populate the list with id "function-list"
 
 // 4. Create a function that takes an array and an age threshold parameter. The function should only display characters whose age is below the given number. Render results in the list with id "age-filter-list"
